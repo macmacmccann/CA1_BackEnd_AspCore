14467ab47d3087ef44fffefc2cfc6c77f5cdae14
Mark McCann <markmccann98@outlook.com>
2026-04-24 14:51:53 +0100

    implemented change to db with migrations

---
+++ CA1_BackEnd/Migrations/20260424135132_changevar.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CA1_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class changevar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsVegetarian",
+                table: "Ingredients",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsVegetarian",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsVegetarian",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsVegetarian",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "IsVegetarian",
+                table: "Ingredients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsVegetarian",
+                value: "false");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "IsVegetarian",
+                value: "true");
+
+            migrationBuilder.UpdateData(
+                table: "Ingredients",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "IsVegetarian",
+                value: "true");
+        }
+    }
+}
